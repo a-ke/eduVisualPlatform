79c196949be3b677c72c181e28ac92cd5d5e3173
huangwenjie <893057523@qq.com>
2018-06-15 17:37:41 +0800

    添加组件样式

---
+++ src/components/borderFrame.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="borderFrame">
+        <span class="frame-horn top-left-horn"></span>
+        <span class="frame-horn top-right-horn"></span>
+        <span class="frame-horn bottom-left-horn"></span>
+        <span class="frame-horn bottom-right-horn"></span>
+    </div>
+</template>
+<script>
+export default {
+  name: "borderFrame"
+};
+</script>
+<style>
+.borderFrame {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  border: 1px solid rgba(7, 140, 235, 0.5);
+  box-shadow: inset 0 0 59px rgba(0, 150, 255, 0.3);
+}
+.frame-horn {
+  position: absolute;
+  display: block;
+  width: 18px;
+  height: 18px;
+  background: url("../../static/images/public/hornIcon.png") no-repeat 100% 100%;
+}
+.frame-horn.top-left-horn {
+  top: -5px;
+  left: -5px;
+}
+.frame-horn.top-right-horn {
+  top: -5px;
+  right: -5px;
+  transform: rotate(90deg);
+}
+.frame-horn.bottom-left-horn {
+  bottom: -5px;
+  left: -5px;
+  transform: rotate(-90deg);
+}
+.frame-horn.bottom-right-horn {
+  bottom: -5px;
+  right: -5px;
+  transform: rotate(180deg);
+}
+</style>
+
---
+++ src/tpl/videoStudio.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="videoStudio">
+        <border-frame class="frameList"></border-frame>
+    </div>
+</template>
+<script>
+const borderFrame = () => import("../components/borderFrame.vue");
+export default {
+  name: "videoStudio",
+  components: {
+    borderFrame
+  }
+};
+</script>
+<style>
+#videoStudio {
+  color: #fff;
+}
+#videoStudio .frameList {
+  width: 200px;
+  height: 800px;
+  margin-top: 100px;
+  margin-left: 100px;
+}
+</style>
+
+
