8c120ba17a2174011ad39f8677899a0415353a24
a-ke <18408902141@163.com>
2018-06-25 19:50:34 +0800

    更改打包的路径，添加公共路径 /visual/

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import 'reset-css'
+import "../static/js/rem"
 import 'element-ui/lib/theme-chalk/index.css'
 import {
   Button,
